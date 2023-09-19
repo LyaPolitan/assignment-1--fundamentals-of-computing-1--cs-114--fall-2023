d4ebd5571f101c2a8633ffb991fca193e4b357e6
Lyam <Lyamperez04@gmail.com>
2023-09-19 13:24:43 -0400

    temp convertor
    i finished the Fahrenheit to Celsius converter i used the other examples to lay the foundation for how i wanted to do this i found the equation and broke it down into peices and then recreated it with different variables

--- Assignment1.java
+++ Assignment1.java
@@ -5,7 +5,16 @@ import java.util.Scanner;
 public class Assignment1 {
   public static void main(String[] args) {
 
-    // Add your code here
+    //declaring all my variables
+    double Celsius;
+    int Fahrenheit;
+    int Base = 32;
+    double Conversion =5.0/9.0;
+
+    //allowing keybaord input
+    Scanner Scan = new Scanner(System.in);
+
+    // My initials
     System.out.println("LLL            PP PPP PPP PP");
     System.out.println("LLL            PPP          PP");
     System.out.println("LLL            PPP          PP");
@@ -13,6 +22,19 @@ public class Assignment1 {
     System.out.println("LLL            PPP");
     System.out.println("LL LLL LLL     PPP");
     System.out.println("LL LLL LLL     PPP");
+    System.out.println("                              ");
 
+    //Asking the user for a temperature
+    System.out.println("please enter a number in Fahrenheit");
+    Fahrenheit = Scan.nextInt();
+
+    //equation that allows me to convert fahrenheit to celsius
+    Celsius = Conversion * (Fahrenheit - Base);
+
+
+    System.out.println("Your temperature in celsius is "+ Celsius);
+
+
+    Scan.close();
   }
 }
