c30d0dbca4a6dd2f793adaf5e51dee157e200626
Lyam <Lyamperez04@gmail.com>
2023-09-19 12:43:59 -0400

    Initials and take backs
    finished the L in my initials and got rid of some half done work in relations to temp converter

--- Assignment1.java
+++ Assignment1.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
+
     // Add your code here
-    System.out.println("LL")
-    System.out.println("LL")
+    System.out.println("LLL");
+    System.out.println("LLL");
+    System.out.println("LLL");
+    System.out.println("LLL");
+    System.out.println("LLL");
+    System.out.println("LL LLL LLL");
+    System.out.println("LL LLL LLL");
+
   }
 }
