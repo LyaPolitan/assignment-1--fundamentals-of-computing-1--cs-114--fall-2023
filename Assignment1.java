b9601627afaf9b67009067c4991e7fada8ecbf62
Lyam <Lyamperez04@gmail.com>
2023-09-19 12:47:18 -0400

    initials
    My initials are done im now going to move onto the temp converter

--- Assignment1.java
+++ Assignment1.java
@@ -6,13 +6,13 @@ public class Assignment1 {
   public static void main(String[] args) {
 
     // Add your code here
-    System.out.println("LLL");
-    System.out.println("LLL");
-    System.out.println("LLL");
-    System.out.println("LLL");
-    System.out.println("LLL");
-    System.out.println("LL LLL LLL");
-    System.out.println("LL LLL LLL");
+    System.out.println("LLL            PP PPP PPP PP");
+    System.out.println("LLL            PPP          PP");
+    System.out.println("LLL            PPP          PP");
+    System.out.println("LLL            PP PPP PPP PP");
+    System.out.println("LLL            PPP");
+    System.out.println("LL LLL LLL     PPP");
+    System.out.println("LL LLL LLL     PPP");
 
   }
 }
