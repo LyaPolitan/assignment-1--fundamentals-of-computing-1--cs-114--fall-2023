dfb139e47202f7d889352e6994350a3c6a788666
Lyam <Lyamperez04@gmail.com>
2023-09-19 12:34:46 -0400

    Added the import needed for keyboard input aswell as started making my initials in big letters

--- Assignment1.java
+++ Assignment1.java
@@ -1,8 +1,11 @@
 // Import libraries here
 // Lyam Perez
+import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println("LL")
+    System.out.println("LL")
   }
 }
